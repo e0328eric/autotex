db599ee4e3db85d6afc6dcf9ba258d13a5e30710
Sungbae Jeong <almagest0328@gmail.com>
2020-07-05 23:53:06 +0900

    autotex

--- src/help.rs
+++ src/help.rs
@@ -1,25 +1,5 @@
-/*
-module AutoTeX.Help where
-
-helpString :: String
-helpString = unlines
-  [
-    "Usage:"
-  , ""
-  , "autotex [-cd] [-v] [-la] [-pdf, -plain, -xe, -lua] FILE.tex"
-  , ""
-  , "Options:"
-  , ""
-  , "-pdf : Execute pdfTeX (-la -pdf : Execute pdfLaTeX)"
-  , "-xe : Execute XeTeX (-la -xe : Execute XeLaTeX)"
-  , "-lua : Execute LuaTeX (-la -lua : Execute LuaLaTeX)"
-  , "-plain : Execute TeX (-la -plain : Execute LaTeX)"
-  , "-v : Open pdf and return the continuous mode"
-  , "-cd : Compile .tex file at the subdirectory"
-  , ""
-  , "Especially, -pdf option can be omitted. (-pdf is the default option)"
-  ]
- */
+// The help string
+// This will displayed when the option "--help"
 pub const HELP_STRING: &str = "Usage:\n
 autotex [-v] [-la] [-pdf, -plain, -xe, -lua] FILENAME\n
 Options:\n
